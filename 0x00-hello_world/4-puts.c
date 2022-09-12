0ba3676c7ddad88c03e65cc1d90957678c946403
Ham-zie <hamzie56@gmail.com>
2022-09-12 09:15:50 -0700

    were

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <main.h>
-
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
 int main(void)
 {
-               int puts; 
-scanf("Programming is like building a multilingual puzzle: %d\n");
-return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
