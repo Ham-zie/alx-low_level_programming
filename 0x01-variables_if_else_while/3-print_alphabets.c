5f4d67724d8349d3489e44627d2219f47d4340ae
Ham-zie <hamzie56@gmail.com>
2022-09-12 16:53:50 -0700

    hytwre

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,24 @@
-#include <stdio.h> 
- /** 
-  * main - main block 
-  * Description:  prints the alphabet in lowercase, 
-  * and then in uppercase, followed by a new line. 
-  * Return: 0 
+include <stdio.h>
+ /**
+  * main - main block
+  * Description:  prints the alphabet in lowercase,
+  * and then in uppercase, followed by a new line.
+  * Return: 0
   */
-int main(void) 
-{ 
-char c = 'a'; 
-while (c <= 'z') 
+int main(void)
 {
-putchar(c); 
-c++; 
-} 
-c = 'A'; 
-while (c <= 'Z') 
+char c = 'a';
+while (c <= 'z')
 {
-putchar(c); 
-c++; 
+putchar(c);
+c++;
 }
-putchar('\n'); 
+c = 'A';
+while (c <= 'Z')
+{
+putchar(c);
+c++;
+}
+putchar('\n');
 return (0);
 }
