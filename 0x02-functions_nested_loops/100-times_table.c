ffbac2fb688e0c62ba0d229e4e045836895774fc
Ham-zie <hamzie56@gmail.com>
2022-09-14 19:43:58 -0700

    All

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,10 +1,12 @@
 #include "main.h"
+#include "holberton.h"
 /**
- * print_times_table - Entry point
+ * Description: print_times_table - Entry point
  * @n: input 
  * Return: Always 0 (Success)
  */
 void print_times_table(int n)
-{
+{	
+	int pld;
 	return (0);
 }
