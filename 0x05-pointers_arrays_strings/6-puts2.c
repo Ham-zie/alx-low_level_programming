87d443ce7bef589ab37453beb3c697801f3bfe9f
Ham-zie <hamzie56@gmail.com>
2022-09-19 19:16:42 -0700

    All

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * Description: puts2 - function should print only one xteracter out of two
+ * starting with the first one
+ * @str: input 
+ * Return: print
+ */
+void puts2(char *str)
+{
+	int lendi = 0;
+	int t = 0;
+	char *y = str;
+	int o;
+	while (*y != '\0')
+	{
+		y++;
+		lendi++;
+	}
+	t = lendi - 1;
+	for (o = 0; o <= t; o++)
+	{
+		if (o % 2 == 0)
+		{
+			_putchar(str[o]);
+		}
+	}
+	_putchar('\n');
+}
