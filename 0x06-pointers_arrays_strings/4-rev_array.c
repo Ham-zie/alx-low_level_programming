dc48c86af03e1e3cf3f191999d8c3868f0ef0514
Ham-zie <hamzie56@gmail.com>
2022-09-21 06:20:21 -0700

    All

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
  * Description: reverse_array - reverse array of integers
  * @a: array
  * @n: number of elements of arrat
- * main: check the code 
+ * main: check the code
  *
  */
 void reverse_array(int *a, int n)
