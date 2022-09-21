76b2827688ff81c6cac3271cae386ff5168125d3
Ham-zie <hamzie56@gmail.com>
2022-09-21 06:19:16 -0700

    All

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * Description: reverse_array - reverse array of integers
+ * @a: array
+ * @n: number of elements of arrat
+ * main: check the code 
+ *
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+	int t;
+	for (i = 0; i < n--; i++)
+	{
+		t = a[i];
+		a[i] = a[n];
+		a[n] = t;
+	}
+}
